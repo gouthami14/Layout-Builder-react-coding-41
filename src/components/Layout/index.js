278bc653943baf23b76a13039ccd3a24b5dd341d
gouthami14 <gouthamivodnala14@gmail.com>
2023-08-11 09:54:54 +0000

    Layout Builder

---
+++ src/components/Layout/index.js
@@ -0,0 +1,17 @@
+import Header from '../Header'
+import Footer from '../Footer'
+import Body from '../Body'
+
+import './index.css'
+
+const Layout = () => (
+  <div>
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  </div>
+)
+
+export default Layout
